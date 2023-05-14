78d909306540a1a5b0996e928c95f8948cad8464
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2023-05-14 17:30:31 +0200

    added handle phases  logic

--- src/components/BreathingApp.jsx
+++ src/components/BreathingApp.jsx
@@ -1,6 +1,27 @@
 import React, { useState, useEffect } from "react";
 
 export default function BreathingApp() {
+  const [currentPhase, setCurrentPhase] = React.useState("inhale");
+  const [timeLeft, setTimeLeft] = React.useState(4);
+  const [isRunning, setIsRunning] = React.useState(false);
+
+  const handleStartStop = () => {
+    setIsRunning(!isRunning);
+  };
+
+  const handleChangePhase = () => {
+    if (currentPhase === "inhale") {
+      setCurrentPhase("hold");
+      setTimeLeft(7);
+    } else if (currentPhase === "hold") {
+      setCurrentPhase("exhale");
+      setTimeLeft(8);
+    } else if (currentPhase === "exhale") {
+      setCurrentPhase("inhale");
+      setTimeLeft(4);
+    }
+  };
+
   return (
     <div>
       <h1>Breathing App Test</h1>
