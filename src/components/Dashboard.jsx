17c6300d8ec68bcb7bbb8cbae5e799c74a7a49dc
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2023-06-17 18:05:53 +0200

    dashboard sections initial version charjs

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { Line } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
+import { userSessionsData } from "../../utils/Data";
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className="dashboard-container">
       <h1>Dashboard Section</h1>
 
       <h2>Your General Stats</h2>
+      <div className="dashboard-graph">
+        <Line data={userSessionsData} />
+      </div>
 
       <h2>Breathing Holding</h2>
+      <div className="dashboard-graph">
+        <Line data={userSessionsData} />
+      </div>
     </div>
   );
 }
---
+++ utils/Data.js
@@ -0,0 +1,12 @@
+export const userSessionsData = {
+  labels: ["Session 1", "Session 2", "Session 3", "Session 4", "Session 5"],
+  datasets: [
+    {
+      label: "User Session",
+      data: [9, 7, 3, 8, 4],
+      backgroundColor: "rgba(90,192,192,0.2)",
+      borderColor: "rgba(90,192,192,1)",
+      borderWidth: 2,
+    },
+  ],
+};
